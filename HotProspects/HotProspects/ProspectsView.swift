7d8139f9f1eee2bd0311f7a517b484720595c162
44gimm <44gimm@gmail.com>
2020-10-22 17:11:34 +0900

    ing: HotProspects 예제 구현중 - TabView - @EnvironmentObject - context menu

---
+++ HotProspects/HotProspects/ProspectsView.swift
@@ -0,0 +1,50 @@
+//
+//  ProspectsView.swift
+//  HotProspects
+//
+//  Created by 44gimm on 2020/10/22.
+//
+
+import SwiftUI
+
+struct ProspectsView: View {
+  
+  enum FilterType {
+    case none, contacted, uncontacted
+  }
+  
+  @EnvironmentObject var prospects: Prospects
+  let filter: FilterType
+  var title: String {
+    switch filter {
+    case .none:
+      return "Everyone"
+    case .contacted:
+      return "Contacted people"
+    case .uncontacted:
+      return "Uncontacted people"
+    }
+  }
+  
+  var body: some View {
+    NavigationView {
+      Text("People: \(prospects.people.count)")
+        .navigationBarTitle(title)
+        .navigationBarItems(trailing: Button(action: {
+          let prospect = Prospect()
+          prospect.name = "Paul Hudson"
+          prospect.emailAddress = "paul@hackingwithswift.com"
+          self.prospects.people.append(prospect)
+        }) {
+          Image(systemName: "qrcode.viewfinder")
+          Text("Scan")
+        })
+    }
+  }
+}
+
+struct ProspectsView_Previews: PreviewProvider {
+  static var previews: some View {
+    ProspectsView(filter: .none)
+  }
+}
