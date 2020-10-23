f58944036e725085dc0f5f7bff859e29b7b68aa1
44gimm <44gimm@gmail.com>
2020-10-23 12:39:32 +0900

    done: HotProspect 예제 - UNUserNotificationCenter

--- HotProspects/HotProspects/ContentView.swift
+++ HotProspects/HotProspects/ContentView.swift
@@ -8,69 +8,12 @@
 import UserNotifications
 import SwiftUI
 
-enum NetworkError: Error {
-  case badURL, requestFailed, unknown
-}
-
 struct ContentView: View {
   
   @State private var backgroundColor = Color.red
   var prospects = Prospects()
   
   var body: some View {
-//    VStack {
-//      Text("Hello world")
-//        .padding()
-//        .background(backgroundColor)
-//
-//      Text("Change Color")
-//        .padding()
-//        .contextMenu {
-//          Button(action: {
-//            self.backgroundColor = .red
-//          }) {
-//            Text("Red")
-//            Image(systemName: "checkmark.circle.fill")
-//              .foregroundColor(.red)
-//          }
-//
-//          Button(action: {
-//            self.backgroundColor = .green
-//          }) {
-//            Text("Green")
-//          }
-//
-//          Button(action: {
-//            self.backgroundColor = .blue
-//          }) {
-//            Text("Blue")
-//          }
-//        }
-//    }
-    
-//    VStack {
-//      Button("Request permission") {
-//        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
-//          if success {
-//            print("All set")
-//          } else if let error = error {
-//            print(error.localizedDescription)
-//          }
-//        }
-//      }
-//
-//      Button("Scehdule Notification") {
-//        let content = UNMutableNotificationContent()
-//        content.title = "Feed the cat"
-//        content.subtitle = "It looks hungry"
-//        content.sound = UNNotificationSound.default
-//        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
-//        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
-//        UNUserNotificationCenter.current().add(request)
-//      }
-//    }
-    
-    
     TabView {
       ProspectsView(filter: .none)
         .tabItem {
@@ -97,26 +40,6 @@ struct ContentView: View {
         }
     }
     .environmentObject(prospects)
-  }
-  
-  func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
-    guard let url = URL(string: urlString) else {
-      completion(.failure(.badURL))
-      return
-    }
-    
-    URLSession.shared.dataTask(with: url) { (data, response, error) in
-      DispatchQueue.main.async {
-        if let data = data {
-          let stringData = String(decoding: data, as: UTF8.self)
-          completion(.success(stringData))
-        } else if error != nil {
-          completion(.failure(.requestFailed))
-        } else {
-          completion(.failure(.unknown))
-        }
-      }
-    }.resume()
   }
 }
 
